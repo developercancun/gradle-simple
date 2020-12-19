5212a51e935c2984680e686d566a197a82f54012
alafita <juancarlosalafita@hotmail.com>
2020-12-19 18:47:55 -0500

    Junit5 support and jacoco

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void tester(){
+        Assertions.assertTrue(true);
+    }
+}
